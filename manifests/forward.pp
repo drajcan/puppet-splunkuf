8663e39e59398390ebe8a40379d34764041eb4b4
drajcan <drajcan@davincisoftware.sk>
2016-05-25 12:47:34 +0200

    [manifests/forward] Added authentication

--- manifests/forward.pp
+++ manifests/forward.pp
@@ -3,12 +3,14 @@ define splunkuf::forward (
   $forward_server = undef,
   $forward_port   = undef,
 
-  $splunk_home = $splunkuf::splunk_home,
+  $splunk_user     = 'admin',
+  $splunk_password = 'changeme',
+  $splunk_home     = $splunkuf::splunk_home,
 
 ){
 
   exec { 'splunk add forward-server':
-    onlyif => "${splunk_home}/bin/splunk add forward-server ${forward_server}:${forward_port}",
+    onlyif => "${splunk_home}/bin/splunk add forward-server ${forward_server}:${forward_port} -auth ${splunk_user}:${splunk_password}",
     path    => ["${splunk_home}/bin", '/bin', '/sbin', '/usr/bin', '/usr/sbin'],
     require => Package['splunkforwarder'],
   }
