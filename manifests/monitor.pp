c812804ae60ae3fda92f5e924a0395fda8766721
drajcan <drajcan@davincisoftware.sk>
2016-05-25 13:38:31 +0200

    [manifests/monitor] Added authentication

--- manifests/monitor.pp
+++ manifests/monitor.pp
@@ -18,7 +18,7 @@ define splunkuf::monitor (
     ensure => 'directory',
   }->
 
-  exec { "${splunk_home}/bin/splunk add monitor ${log_path} -index main -sourcetype %app%":
+  exec { "${splunk_home}/bin/splunk add monitor ${log_path} -index main -sourcetype %app% -auth ${splunk_user}:${splunk_password}":
     path    => ["${splunk_home}/bin", '/bin', '/sbin', '/usr/bin', '/usr/sbin'],
     creates => "${splunk_home}/monitors/${log_path}",
     require => Package['splunkforwarder'],
