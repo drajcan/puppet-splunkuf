9f490e3ed2f8130b84e5436e597bdc877cf81516
drajcan <drajcan@davincisoftware.sk>
2016-05-25 13:43:01 +0200

    [manifests/monitor] Modified

--- manifests/monitor.pp
+++ manifests/monitor.pp
@@ -14,17 +14,17 @@ define splunkuf::monitor (
     mode  => '0644',
   }
 
-  file {"${splunk_home}/monitors/":
+  file {"${splunk_home}/monitors":
     ensure => 'directory',
   }->
 
   exec { "${splunk_home}/bin/splunk add monitor ${log_path} -index main -sourcetype %app% -auth ${splunk_user}:${splunk_password}":
     path    => ["${splunk_home}/bin", '/bin', '/sbin', '/usr/bin', '/usr/sbin'],
-    creates => "${splunk_home}/monitors/${log_path}",
+    creates => "${splunk_home}/monitors${log_path}",
     require => Package['splunkforwarder'],
   }->
 
-  file { "${splunk_home}/monitors/${log_path}":
+  file { "${splunk_home}/monitors${log_path}":
     ensure => 'present',
   }
 
