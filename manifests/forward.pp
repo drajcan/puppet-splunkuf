3d7d2430f2f7e5c702afef0f1489dcd7e039993c
drajcan <drajcan@davincisoftware.sk>
2016-05-25 12:58:53 +0200

    [manifests/forward] Changed resource identificator

--- manifests/forward.pp
+++ manifests/forward.pp
@@ -9,7 +9,7 @@ define splunkuf::forward (
 
 ){
 
-  exec { 'splunk add forward-server':
+  exec { "${forward_server}:${forward_port}":
     onlyif => "${splunk_home}/bin/splunk add forward-server ${forward_server}:${forward_port} -auth ${splunk_user}:${splunk_password}",
     path    => ["${splunk_home}/bin", '/bin', '/sbin', '/usr/bin', '/usr/sbin'],
     require => Package['splunkforwarder'],
