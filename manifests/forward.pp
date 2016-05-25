0265582a0f1338840193ffd503d4e4ea85dec2b9
drajcan <drajcan@davincisoftware.sk>
2016-05-25 13:36:47 +0200

    Fixed typo

--- manifests/forward.pp
+++ manifests/forward.pp
@@ -21,7 +21,7 @@ define splunkuf::forward (
 
   exec { "${splunk_home}/bin/splunk add forward-server ${forward_server}:${forward_port} -auth ${splunk_user}:${splunk_password}":
     path    => ["${splunk_home}/bin", '/bin', '/sbin', '/usr/bin', '/usr/sbin'],
-    creates => "${splunk_home}/${forward_server}",
+    creates => "${splunk_home}/forwarders/${forward_server}",
     require => Package['splunkforwarder'],
   }->
 
