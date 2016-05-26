cd59d70fde13d3fe3a114b632665e400ae8ed7a1
drajcan <drajcan@davincisoftware.sk>
2016-05-26 10:06:22 +0200

    [manifests] Modfiied dependencies

--- manifests/monitor.pp
+++ manifests/monitor.pp
@@ -19,7 +19,6 @@ define splunkuf::monitor (
   exec { "${splunk_home}/bin/splunk add monitor ${log_path} -index main -sourcetype %app% -auth ${splunk_user}:${splunk_password}":
     path    => ["${splunk_home}/bin", '/bin', '/sbin', '/usr/bin', '/usr/sbin'],
     creates => "${splunk_home}/monitors/${name}",
-    notify  => Service['splunkforwarder'],
     require => Class['splunkuf'],
   }->
 
